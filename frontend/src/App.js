6f7229df766e6a0a615e5323700a27941fbc1562
flydotxxx <prod@fly.xxx>
2026-06-17 16:22:10 +0200

    Fix: Replace @ aliases with relative imports for react-scripts compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { LanguageProvider } from "@/context/LanguageContext";
-import { Layout } from "@/components/Layout";
-import HomePage from "@/pages/HomePage";
-import BrandsPage from "@/pages/BrandsPage";
-import FoundersPage from "@/pages/FoundersPage";
-import OperationsPage from "@/pages/OperationsPage";
-import AboutPage from "@/pages/AboutPage";
+import { LanguageProvider } from "./context/LanguageContext";
+import { Layout } from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import BrandsPage from "./pages/BrandsPage";
+import FoundersPage from "./pages/FoundersPage";
+import OperationsPage from "./pages/OperationsPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
